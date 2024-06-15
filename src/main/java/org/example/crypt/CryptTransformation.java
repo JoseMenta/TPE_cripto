333e49333c084a161ebc3619c6b828bc02cd4d47
Axel Preiti Tasat <axel.preitit@gmail.com>
2024-06-15 19:41:41 -0300

    feat: parametrize crypt padding

--- src/main/java/org/example/crypt/CryptTransformation.java
+++ src/main/java/org/example/crypt/CryptTransformation.java
@@ -1,17 +1,18 @@
 package org.example.crypt;
 
 public class CryptTransformation {
-    private static final String NO_PADDING = "NoPadding";
 
     private final CryptAlgorithm cryptAlgorithm;
     private final CryptMode cryptMode;
+    private final CryptPadding cryptPadding;
 
-    public CryptTransformation(CryptAlgorithm cryptAlgorithm, CryptMode cryptMode) {
+    public CryptTransformation(CryptAlgorithm cryptAlgorithm, CryptMode cryptMode, CryptPadding cryptPadding) {
         this.cryptAlgorithm = cryptAlgorithm;
         this.cryptMode = cryptMode;
+        this.cryptPadding = cryptPadding;
     }
 
     public String getTransformation() {
-        return cryptAlgorithm.getAlgorithm() + "/" + cryptMode.getMode() + "/" + NO_PADDING;
+        return cryptAlgorithm.getAlgorithm() + "/" + cryptMode.getMode() + "/" + cryptPadding.getPadding();
     }
 }
