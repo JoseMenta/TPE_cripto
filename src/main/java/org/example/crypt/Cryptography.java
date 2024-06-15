39f7e5c48cb60c108379b01de2940ce1f105b554
Axel Preiti Tasat <axel.preitit@gmail.com>
2024-06-15 19:42:01 -0300

    chore: include iv in cipher init

--- src/main/java/org/example/crypt/Cryptography.java
+++ src/main/java/org/example/crypt/Cryptography.java
@@ -6,12 +6,13 @@ import javax.crypto.NoSuchPaddingException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 public interface Cryptography {
-    void encrypt(InputStream in, OutputStream out) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, IOException, BadPaddingException;
+    void encrypt(InputStream in, OutputStream out) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, IOException, BadPaddingException, InvalidAlgorithmParameterException;
 
-    void decrypt(InputStream in, OutputStream out) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, IOException, BadPaddingException;
+    void decrypt(InputStream in, OutputStream out) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, IOException, BadPaddingException, InvalidAlgorithmParameterException;
 }
 
--- src/main/java/org/example/crypt/CryptographyImpl.java
+++ src/main/java/org/example/crypt/CryptographyImpl.java
@@ -4,9 +4,11 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
@@ -16,10 +18,12 @@ public class CryptographyImpl implements Cryptography {
 
     private final CryptTransformation cryptTransformation;
     private final Key key;
+    private final IvParameterSpec iv;
 
-    public CryptographyImpl(CryptTransformation cryptTransformation, Key key) {
+    public CryptographyImpl(CryptTransformation cryptTransformation, Key key, IvParameterSpec iv) {
         this.cryptTransformation = cryptTransformation;
         this.key = key;
+        this.iv = iv;
     }
 
     private void execute(Cipher cipher, InputStream in, OutputStream out) throws IOException, IllegalBlockSizeException, BadPaddingException {
@@ -38,16 +42,16 @@ public class CryptographyImpl implements Cryptography {
     }
 
     @Override
-    public void encrypt(InputStream in, OutputStream out) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, IOException, BadPaddingException {
+    public void encrypt(InputStream in, OutputStream out) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, IOException, BadPaddingException, InvalidAlgorithmParameterException {
         Cipher cipher = Cipher.getInstance(cryptTransformation.getTransformation());
-        cipher.init(Cipher.ENCRYPT_MODE, key);
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
         execute(cipher, in, out);
     }
 
     @Override
-    public void decrypt(InputStream in, OutputStream out) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, IOException, BadPaddingException {
+    public void decrypt(InputStream in, OutputStream out) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, IOException, BadPaddingException, InvalidAlgorithmParameterException {
         Cipher cipher = Cipher.getInstance(cryptTransformation.getTransformation());
-        cipher.init(Cipher.DECRYPT_MODE, key);
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
         execute(cipher, in, out);
     }
 }
