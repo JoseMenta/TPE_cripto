96a422222a9cf8c0a17cd0acbbe2be6f5899b595
Martin Hecht <mhecht@itba.edu.ar>
2024-06-15 19:29:03 -0300

    chore: cleanup LSB1

--- src/main/java/org/example/algorithm/LSB1.java
+++ src/main/java/org/example/algorithm/LSB1.java
@@ -23,18 +23,20 @@ public class LSB1 implements Algorithm{
         byte[] sizeArray = payload.getSizeBinary();
         byte[] bmpData = bmp.getData();
 
+        //HEADER
         for (int i = 0; i < SIZE_BITS; i++) {
             int bitIndex = i % BYTE_SIZE;
             int byteIndex = i / BYTE_SIZE;
             byte targetByte = sizeArray[byteIndex];
             //FIX: sería (targetByte >> (7-bitIndex)), porque el primer bit es el que se obtiene haciendo 7 shifts a la derecha
-            int targetBit = (targetByte >> (7- bitIndex)) & 0x01;
+            int targetBit = (targetByte >> (7 - bitIndex)) & 0x01;
             bmpData[i] = (byte) ((bmpData[i] & 0xFE) | targetBit);
         }
 
         byte[] payloadData = payload.getContent();
         int offset = SIZE_BITS;
 
+        //BODY
         for (int i = 0; i < payloadData.length; i++) {
             for (int j = 0; j < BYTE_SIZE; j++) {
                 byte targetByte = payloadData[i];
@@ -45,17 +47,19 @@ public class LSB1 implements Algorithm{
             }
         }
 
+        //EXTENSION
         byte[] extension = payload.getExtensionBinary();
         for (int i = 0; i < extension.length; i++) {
             for (int j = 0; j < BYTE_SIZE; j++) {
                 byte targetByte = extension[i];
                 // FIX: Misma correccion que arriba
-                int targetBit = (targetByte >> (7-j)) & 0x01;
+                int targetBit = (targetByte >> (7 - j)) & 0x01;
                 bmpData[offset] = (byte) ((bmpData[offset] & 0xFE) | targetBit);
                 offset++;
             }
         }
 
+        // Final 0
         for (int i = 0; i < BYTE_SIZE; i++) {
             bmpData[offset] = (byte) (bmpData[offset] & 0xFE);
             offset++;
@@ -79,7 +83,7 @@ public class LSB1 implements Algorithm{
         for (int i = 0; i < Integer.BYTES; i++) {
             sizeBinary[i] = 0;
             for (int j = 0; j < BYTE_SIZE; j++) {
-                sizeBinary[i] = (byte) ((sizeBinary[i] << 1) | (porterData[p] & 0x01));
+                sizeBinary[i] = (byte) ((sizeBinary[i] << 0x01) | (porterData[p] & 0x01));
                 p++;
             }
         }
@@ -92,7 +96,7 @@ public class LSB1 implements Algorithm{
         for (int i = 0; i < size; i++) {
             content[i] = 0;
             for (int j = 0; j < BYTE_SIZE; j++) {
-                content[i] = (byte) ((content[i] << 1) | (porterData[p] & 0x01));
+                content[i] = (byte) ((content[i] << 0x01) | (porterData[p] & 0x01));
                 p++;
             }
         }
@@ -103,10 +107,10 @@ public class LSB1 implements Algorithm{
             byte last = 0;
             do{
                 for (int j = 0; j < BYTE_SIZE; j++) {
-                    last = (byte) ((last << 1) | (porterData[p] & 0x01));
+                    last = (byte) ((last << 0x01) | (porterData[p] & 0x01));
                     p++;
                 }
-                if(last!=0){
+                if(last != 0){
                     builder.append((char) last);
                 }
             } while (last != 0);
